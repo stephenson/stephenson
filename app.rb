b7e36536a860df7a971bb6b95011f3daf4833329
Niklas Stephenson <ns@firmafon.dk>
2011-04-10 21:45:20 +0200

    Ruby syntaxt

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 
-// Incons http://www.iconfinder.com/search/?q=iconset%3AMacchiato_Social
+# Incons http://www.iconfinder.com/search/?q=iconset%3AMacchiato_Social
 get '/' do
   erb :forside
 end
